177b277a24d8e40f9125c94851e9f4d613625d5e
Jim <jimmybnguyen@yahoo.com>
2020-02-19 14:48:49 -0800

    toggle boxes created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [appState, changeState] = useState({
+    activeObject: null,
+    objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+  });
+
+  function toggleActive(index) {
+    changeState({ ...appState, activeObject: appState.objects[index] });
+  }
+
+  function toggleActiveStyles(index) {
+    if (appState.objects[index] === appState.activeObject) {
+      return "box active";
+    } else {
+      return "box inactive";
+    }
+  }
+
   return (
     <div className="App">
-      <h1>
-        Hello World. Added text to see if i have to also push to heroku origin
-        master too?
-      </h1>
+      {appState.objects.map((elements, index) => (
+        <div
+          key={index}
+          className={toggleActiveStyles(index)}
+          onClick={() => {
+            toggleActive(index);
+          }}
+        ></div>
+      ))}
     </div>
   );
 }
